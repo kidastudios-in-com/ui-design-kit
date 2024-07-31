e3558f9093abc2ddb4ea19a08a55506e49d65a28
samagyaanand <samagyaanand2109@gmail.com>
2024-07-31 16:02:15 +0530

    creates glass effect

--- lib/glass_effect.dart
+++ lib/glass_effect.dart
@@ -3,13 +3,17 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class GlassEffect extends StatelessWidget {
-  const GlassEffect({super.key});
+  final height;
+  final width;
+  const GlassEffect({super.key , required this.height, required this.width});
 
   @override
   Widget build(BuildContext context) {
     return BackdropFilter(
                 filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
                 child: Container(
+                  height: height,
+                  width: width,
                   decoration: BoxDecoration(
                     color: Colors.white.withOpacity(0.2),
                     border: Border.all(
